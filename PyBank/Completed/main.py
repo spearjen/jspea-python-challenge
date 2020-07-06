f7e0d9d83994586e6c5858651f87b2e7c39b918c
Jay Spears <j.spears.denver@gmail.com>
2020-07-06 16:17:07 -0600

    Final formatted txt

---
+++ PyBank/Completed/main.py
@@ -0,0 +1,74 @@
+
+#Import modules
+import os
+import csv
+
+#Zero totals
+total = 0
+changetotal = 0
+currenthighchange = 0
+currentlowchange = 0
+
+#Define file path
+csvpath = os.path.join("..","Resources","budget_data.csv" )
+
+#Read CSV file
+with open(csvpath,"r") as csvfile:
+    csvreader = csv.reader(csvfile)
+    #Skip the header row 
+    csvheader = next(csvreader)
+    #Count number of lines in CSV file, and print info using f string
+    lines = len(list(csvreader))
+   
+#Read CSV files
+with open(csvpath,"r") as csvfile:
+    csvreader = csv.reader(csvfile)
+    #Skip the header row 
+    csvheader = next(csvreader)
+    #Iterate through rows and set initial values
+    for row in csv.reader(csvfile):
+        bestdate = row[0]
+        worstdate = row[0]
+        old = float(row[1])
+        total = float(row[1])
+        oldchange =float(0)    
+        #Iterate through rows, finding total change
+        for row in csv.reader(csvfile):
+            total = float(row[1]) + total
+            new = float(row[1])
+            change = new-old
+            old = new
+            changetotal = float(changetotal) + float(change)
+            #Iterate through rows, finding greatest change and least change (month to month)
+            if currenthighchange >= change:
+                currenthighchange = currenthighchange
+            else:
+                currenthighchange = change
+                bestdate = str(row[0])
+            
+            if currentlowchange <= change:
+                currentlowchange = currentlowchange
+            else:
+                currentlowchange = change
+                worstdate = str(row[0])
+        #Format to currency   
+        #Currency formatting info found at kite.com
+        currencytotal = "${:,.2f}".format(total)
+        currencychangetotal = "${:,.2f}".format(changetotal/(lines-1))     
+        currencyhigh = "${:,.2f}".format(currenthighchange)
+        currencylow = "${:,.2f}".format(currentlowchange)
+        
+    data = (f"There are {lines} months of data contained in the dataset.\n"
+            f"The total profit/loss for all months and years in the dataset is: {currencytotal}.\n"
+            f"The average profit/loss change between months in the dataset is: {currencychangetotal}.\n"
+            f"The greatest increase from one month to another was in {bestdate} for a gain of {currencyhigh}.\n"
+            f"The greatest decrease from one month to another was in {worstdate} for a loss of {currencylow}.")
+   
+    #Print to screen
+    print(data)
+
+    #Print to file
+    outputfile = os.path.join ("..","Analysis","bank_final.txt")
+    with open(outputfile, "w") as bank_final:
+        bank_final.write(data)
+        
---
+++ PyPoll/Completed/main.py
@@ -0,0 +1,94 @@
+#Import modules
+import os
+import csv
+
+#Zero totals
+Khan = 0
+Correy = 0
+Li = 0
+OTooley = 0
+
+#Set path
+csvpath = os.path.join("..","Resources","election_data.csv" )
+
+#Read CSV file
+with open(csvpath,"r") as csvfile:
+    csvreader = csv.reader(csvfile)
+    #Skip the header row 
+    csvheader = next(csvreader)
+    #Count number of lines in CSV file, and print info using f string
+    lines = len(list(csvreader))
+    print(f"The total number of votes cast was: {lines}")
+    print("________________________________________________")
+   
+#Read CSV files
+with open(csvpath,"r") as csvfile:
+    csvreader = csv.reader(csvfile)
+    #Skip the header row 
+    csvheader = next(csvreader)
+
+    vote_dict = {}
+
+    #Iterate through rows and find total number of votes
+    for row in csvreader:
+    #Create dictionary that keys unique candidates and finds vote totals at the same time; I learned this from awesome Justin in my tutoring session!
+        try:
+            vote_dict[row[2]] += 1
+        except:
+            vote_dict[row[2]] = 1
+    #Stardard method iterating through rows
+        vote = row[2]
+        if vote == "Khan":
+            Khan = Khan + 1
+        elif vote == "Correy":
+            Correy = Correy + 1
+        elif vote == "Li":
+            Li = Li + 1
+        elif vote == "O'Tooley":
+            OTooley = OTooley + 1
+    print(vote_dict)
+    
+    #Find percentages
+    Khanper = Khan/lines
+    Correyper = Correy/lines
+    Liper=Li/lines
+    OTooleyper = OTooley/lines
+
+    #Format percentages
+    #Percentage formatting info found at kite.com
+    Khanpercentage = "{:.0%}".format(Khanper)
+    Correypercentage = "{:.0%}".format(Correyper)
+    Lipercentage = "{:.0%}".format(Liper)
+    OTooleypercentage = "{:.0%}".format(OTooleyper)
+
+    #Find the winner
+    #Find largest number in list info found at tutorialgateway.org
+    votetotals = [Khan, Correy, Liper, OTooley]
+    winner = ""
+    if (max(votetotals)) == Khan:
+        winner = "Khan "
+    elif (max(votetotals)) == Correy:
+        winner = "Correy "
+    elif (max(votetotals)) == Li:
+        winner = "Li "
+    elif (max(votetotals)) == OTooley:
+        winner = "O'Tooley "
+
+#Print to file
+    data = (f"The total number of votes cast was: {lines}\n"
+            "________________________________________________\n"
+            f"Khan received {Khanpercentage} ({Khan}) of votes\n"
+            f"Correy received {Correypercentage} ({Correy}) of votes\n"
+            f"Li received {Lipercentage} ({Li}) of votes\n"
+            f"O'Tooley received {OTooleypercentage} ({OTooley}) of votes\n"
+            "________________________________________________\n"
+            f"{winner}wins!!!")
+    #Print to screen
+    print(data)
+    
+    #Print to csv
+    outputfile = os.path.join ("..","Analysis","poll_final.txt")
+    with open(outputfile, "w") as poll_final:
+        poll_final.write(data)
+        
+    
